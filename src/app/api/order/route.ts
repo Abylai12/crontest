3b7116093694d951941bf1270baa33c0d39112fb
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2025-01-07 16:16:03 +0800

    first commit

---
+++ src/app/api/order/route.ts
@@ -0,0 +1,20 @@
+import Order from "@/models/order.model";
+import { connectToDb } from "@/utils";
+
+export async function GET(request: Request) {
+  await connectToDb();
+  const currentDate = new Date();
+  console.log("request", request);
+  try {
+    await Order.deleteMany({
+      payment: "pending",
+      createdAt: { $lt: new Date(currentDate.getTime() - 2 * 60 * 1000) },
+    });
+    return new Response("Successfully deleted orders older than 2 minutes.", {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error occurred while processing orders:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
